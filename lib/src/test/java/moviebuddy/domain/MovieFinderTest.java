c595fc0b5b30d52962c4d3ac39889593b8528f7f
Gyeha Lim <skywno@gmail.com>
2022-08-03 14:25:37 +0300

    Section 4: Add object factory

--- lib/src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ lib/src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -2,18 +2,30 @@ package moviebuddy.domain;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import moviebuddy.MovieBuddyFactory;
+
 /**
  * @author springrunner.kr@gmail.com
  */
 public class MovieFinderTest {
 
-	public static void main(String[] args) {
-		MovieFinder movieFinder = new MovieFinder();
-		List<Movie> result = movieFinder.directedBy("Michael Bay");
-		assertEquals(3, result.size());
-
-        result = movieFinder.releasedYearBy(2015);
-        assertEquals(225, result.size());
+	final MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();
+	final MovieFinder movieFinder = movieBuddyFactory.movieFinder();		
+	
+	@Test
+	void NotEMpty_directedBy() {
+		List<Movie> movies = movieFinder.directedBy("Michael Bay");
+		Assertions.assertEquals(3, movies.size());
+	}
+	
+	@Test
+	void NotEmpty_ReleasedYearBy() {
+		List<Movie> movies = movieFinder.releasedYearBy(2015);
+		Assertions.assertEquals(225, movies.size());
+		
 	}
 	
 	static void assertEquals(long expected, long actual) {
